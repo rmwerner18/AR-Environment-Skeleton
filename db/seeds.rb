49248158176e0a912a8d6b1776b767ea6c504e0f
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-23 16:39:39 -0400

    round 2 final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Student.delete_all
 Teacher.delete_all
 
-john = Student.create(first_name: "John", last_name: "Doe", grade_level: "tenth")
-mary = Student.create(first_name: "Mary", last_name: "Sue", grade_level: "eleventh")
-
 johnson = Teacher.create(last_name: "Johnson", grade_level: "tenth", years_of_experience: 10)
 white = Teacher.create(last_name: "White", grade_level: "tenth", years_of_experience: 4)
+john = Student.create(first_name: "John", last_name: "Doe", grade_level: "tenth", teacher: johnson)
+mary = Student.create(first_name: "Mary", last_name: "Sue", grade_level: "eleventh", teacher: white)
+Student.create(first_name: "Phil", last_name: "Phillips", grade_level: "tenth", teacher: johnson)
